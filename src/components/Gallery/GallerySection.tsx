d7f9e66abc4545760792de46a573a6d835da9436
hernanda🧙 <m.hernanda95@gmail.com>
2025-07-25 15:42:06 +0700

    feeding folder

--- src/components/Gallery/GallerySection.tsx
+++ src/components/Gallery/GallerySection.tsx
@@ -20,31 +20,31 @@ const GallerySection: React.FC<GallerySectionProps> = ({ lang = 'en' }) => {
   // List of all folders and their images
   const galleryGroups = [
     {
-      name: 'Ziarah_Kasih_2_Buah_Rumah_Kg_Paya_Ikan',
+      name: '01',
       images: [
         'DSC04994.jpg','DSC04996.jpg','DSC04997.jpg','DSC04998.jpg','DSC05001.jpg','DSC05002.jpg','DSC05003.jpg','DSC05007.jpg','DSC05009.jpg','DSC05010.jpg','DSC05011.jpg','DSC05013.jpg','DSC05016.jpg','DSC05019.jpg','DSC05020.jpg','DSC05022.jpg','DSC05024.jpg','DSC05025.jpg','DSC05028.jpg','DSC05029.jpg','DSC05030.jpg','DSC05031.jpg','DSC05035.jpg','DSC05039.jpg','DSC05044.jpg','DSC05049.jpg','DSC05051.jpg','DSC05055.jpg'
       ]
     },
     {
-      name: 'Ziarah_Kasih_Bukit_Katil_Indah_Kg_Bukit_Pau',
+      name: '02',
       images: [
         'DSC01296.jpg','DSC01299.jpg','DSC01302.jpg','DSC01304.jpg','DSC01306.jpg','DSC01308.jpg','DSC01310.jpg','DSC01320.jpg','DSC01322.jpg','DSC01324.jpg','DSC01329.jpg','DSC01331.jpg','DSC01334.jpg','DSC01337.jpg','DSC01339.jpg','DSC01341.jpg','DSC01342.jpg','DSC01343.jpg','DSC01349.jpg','DSC01352.jpg','DSC01355.jpg','DSC01357.jpg','DSC01361.jpg','DSC01362.jpg','DSC01363.jpg','DSC01365.jpg','DSC01369.jpg','DSC01370.jpg'
       ]
     },
     {
-      name: 'Ziarah_Kasih_Bukit_Piatu',
+      name: '03',
       images: [
         'DSC02657.jpg','DSC02658.jpg','DSC02659.jpg','DSC02661.jpg','DSC02664.jpg','DSC02666.jpg','DSC02668.jpg','DSC02671.jpg','DSC02674.jpg','DSC02676.jpg','DSC02677.jpg','DSC02679.jpg','DSC02681.jpg','DSC02683.jpg','DSC02685.jpg','DSC02687.jpg','DSC02689.jpg','DSC02692.jpg','DSC02693.jpg','DSC02696.jpg','DSC02698.jpg','DSC02700.jpg','DSC02702.jpg','DSC02704.jpg','DSC02706.jpg','DSC02708.jpg','DSC02710.jpg','DSC02711.jpg','DSC02714.jpg'
       ]
     },
     {
-      name: 'Ziarah_Kasih_Bukit_Piatu_Kg_Enam_Tmn_RB',
+      name: '04',
       images: [
         'DSC08821.jpg','DSC08823.jpg','DSC08825.jpg','DSC08826.jpg','DSC08828.jpg','DSC08830.jpg','DSC08834.jpg','DSC08837.jpg','DSC08841.jpg','DSC08843.jpg','DSC08845.jpg','DSC08847.jpg','DSC08851.jpg','DSC08853.jpg','DSC08856.jpg','DSC08860.jpg','DSC08861.jpg','DSC08864.jpg','DSC08866.jpg','DSC08868.jpg','DSC08872.jpg','DSC08874.jpg','DSC08878.jpg','DSC08880.jpg','DSC08882.jpg','DSC08885.jpg','DSC08887.jpg','DSC08888.jpg','DSC08890.jpg','DSC08893.jpg','DSC08896.jpg','DSC08899.jpg','DSC08902.jpg','DSC08906.jpg'
       ]
     },
     {
-      name: 'Ziarah_Kasih_Kg_Bukit_Katil',
+      name: '05',
       images: [
         'DSC03332.jpg','DSC03333.jpg','DSC03336.jpg','DSC03339.jpg','DSC03341.jpg','DSC03343.jpg','DSC03345.jpg','DSC03347.jpg','DSC03348.jpg','DSC03350.jpg','DSC03352.jpg','DSC03354.jpg','DSC03356.jpg','DSC03358.jpg','DSC03360.jpg'
       ]
